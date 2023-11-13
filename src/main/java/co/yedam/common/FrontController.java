55ae6d96db061b83abcf8a202e3280db046b79b8
branch <dd.com>
2023-11-13 15:59:16 +0900

    fffff

--- src/main/java/co/yedam/common/FrontController.java
+++ src/main/java/co/yedam/common/FrontController.java
@@ -14,25 +14,27 @@ import co.yedam.order.GoOrderControl;
 
 public class FrontController extends HttpServlet {
 	Map<String, command> map = new HashMap<>();
-	
+
 	@Override
 	public void init() throws ServletException {
-		map.put("/main.do", new GoMainControl());		
-		map.put("/cart.do", new GoCartControl());		
-		map.put("/order.do", new GoOrderControl());		
-		map.put("/order.do", new GoOrderControl());		
-		
+		map.put("/main.do", new GoMainControl());
+		map.put("/cart.do", new GoCartControl());
+		map.put("/order.do", new GoOrderControl());
+		map.put("/order.do", new GoOrderControl());
+
+		// hhhhhh
+
 	}
-	
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html; charset=utf-8");
-		
+
 		String page = req.getServletPath();
-		
+
 		command controller = map.get(page);
 		controller.execute(req, resp);
 	}
-	
+
 }
