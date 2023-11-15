e5a9362688de0fb7ed61b00c590129d1782fed0c
SungohYoon <nsa30003@gmail.com>
2023-11-15 17:53:54 +0900

    [리뷰작성 (50/100)%] 11-15 윤성오

--- src/main/java/co/yedam/review/web/GoAddReviewControl.java
+++ src/main/java/co/yedam/review/web/GoAddReviewControl.java
@@ -1,6 +1,7 @@
 package co.yedam.review.web;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,17 @@ public class GoAddReviewControl implements command {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
 		String path = "/review/addReview.tiles";
+		ReviewService svc = new ReviewServiceImpl();
+		
+		String menuName = req.getParameter("menuName"); // 상품명
+		String reviewContent = req.getParameter("reviewContent"); // 리뷰내용
+		String userId = req.getParameter("userId"); // 아이디
+		String reviewImage = req.getParameter("reviewImage"); // 이미지
+		int reviewStar = Integer.parseInt(req.getParameter("reviewStar")); // 별점
+		int reviewBoomup = Integer.parseInt(req.getParameter("reviewBoomup")); // 추천수
+		String reviewDate = (req.getParameter("reviewImage")); // 날짜
+		
+		
 		try {
 			req.getRequestDispatcher(path).forward(req, resp);
 		} catch (ServletException | IOException e) {
