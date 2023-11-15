04dd569d2bd3942d0050b87849f7b70467e644d7
DESKTOP-SVINUC9\admin <ff>
2023-11-15 14:38:04 +0900

    11-15 에러 삭제 버전 재혀ㅓㄴ

--- src/main/java/co/yedam/common/MainExe.java
+++ src/main/java/co/yedam/common/MainExe.java
@@ -1,11 +1,15 @@
 package co.yedam.common;
 
+import org.apache.ibatis.session.SqlSession;
+
+import co.yedam.menu.mapper.MenuMapper;
+
 public class MainExe {
 
 	public static void main(String[] args) {
 
 		SqlSession session = DataSourceMybatis.getInstance().openSession(true);
 		MenuMapper mapper = session.getMapper(MenuMapper.class);
-	
 
+	}
 }
