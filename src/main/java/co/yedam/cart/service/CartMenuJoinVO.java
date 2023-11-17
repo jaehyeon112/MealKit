d50952b9064fe3e14a198b7aa5473ece5d0d4616
DESKTOP-SVINUC9\admin <ff>
2023-11-17 17:34:37 +0900

    재현

---
+++ src/main/java/co/yedam/cart/service/CartMenuJoinVO.java
@@ -0,0 +1,11 @@
+package co.yedam.cart.service;
+
+import lombok.Data;
+
+@Data
+public class CartMenuJoinVO {
+	private String userId;
+	private int price;
+	private int priceOff;
+	private int total;
+}
--- src/main/java/co/yedam/common/MainExe.java
+++ src/main/java/co/yedam/common/MainExe.java
@@ -1,21 +1,21 @@
 package co.yedam.common;
 
+
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 
 import co.yedam.cart.mapper.CartMapper;
+import co.yedam.cart.service.CartMenuJoinVO;
 import co.yedam.cart.service.CartService;
-import co.yedam.cart.service.CartVO;
 import co.yedam.cart.serviceImpl.CartServiceImpl;
-import co.yedam.menu.service.MenuService;
-import co.yedam.menu.service.MenuVO;
-import co.yedam.menu.serviceImpl.MenuServiceImpl;
-
 public class MainExe {
 
 	public static void main(String[] args) {
-		CartService svc = new CartServiceImpl();
-		Integer a = svc.checkCartList("112");
-		System.out.println(a);
+		
+		SqlSession session = DataSourceMybatis.getInstance().openSession(true);
+		CartMapper mapper = session.getMapper(CartMapper.class);
+		
+		CartMenuJoinVO vo = mapper.joinCartMenu("12345");
+		System.out.println(vo);
+		
 	}
 }
