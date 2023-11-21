90f486ea2ff312a6415e677b15fe64fda02becf7
main <amikjh@naver.com>
2023-11-21 22:53:56 +0900

    장바구니 -> 결제

--- src/main/java/co/yedam/order/mapper/OrderMapper.java
+++ src/main/java/co/yedam/order/mapper/OrderMapper.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Param;
 
 import co.yedam.cart.service.CartVO;
+import co.yedam.order.service.OrderDetailVO;
+import co.yedam.order.service.OrdersVO;
 
 public interface OrderMapper {
 	//@Param("cartArr")String[] cartNum, @Param("userId")String userId
@@ -13,4 +15,7 @@ public interface OrderMapper {
 	public String gainPoint(Map<String, Object> map);
 	public int UpdateUserinfoAfterdecideOrder(Map<String, Object> map);
 	public int menuUpdate(@Param("menuId")String menuId, @Param("menuCount")int menuCount);
+	public int deleteCart(int cartNum);
+	public int insertOrder(OrdersVO vo);
+	public int insertOrderDetail(OrderDetailVO vo);
 }
