a58f36c8099f915dd9bda49b1e7ae032d04365ce
SungohYoon <nsa30003@gmail.com>
2023-11-21 16:27:57 +0900

    [리뷰 수정] 윤성오 23-11-21

--- src/main/java/co/yedam/review/web/ModMyReviewFormControl.java
+++ src/main/java/co/yedam/review/web/ModMyReviewFormControl.java
@@ -1,12 +1,12 @@
-package co.yedam.review.web;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+ package co.yedam.review.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.yedam.common.command;
+import co.yedam.review.service.ReviewService;
+import co.yedam.review.service.ReviewVO;
+import co.yedam.review.serviceImpl.ReviewServiceImpl;
 
 public class ModMyReviewFormControl implements command {
 
@@ -15,12 +15,20 @@ public class ModMyReviewFormControl implements command {
 		
 		int orderDetailNumber = Integer.parseInt(req.getParameter("orderDetailNumber"));
 //		String reviewDate = req.getParameter("reviewDate");
-		String userId = req.getParameter("userId");
-		String menuName = req.getParameter("menuName");
-		req.setAttribute("userId", userId);
-		req.setAttribute("orderDetailNumber", orderDetailNumber);
-//		req.setAttribute("reviewDate", reviewDate);
-		req.setAttribute("menuName", menuName);
+		ReviewService svc = new ReviewServiceImpl();
+		ReviewVO vo = svc.selectOne(orderDetailNumber);
+		System.out.println(vo);
+		req.setAttribute("vo", vo);
+		
+		
+//		String test = req.getParameter("date");
+//		System.out.println(test);
+//		String userId = req.getParameter("userId");
+//		String menuName = req.getParameter("menuName");
+//		vo.setUserId(userId);
+//		req.setAttribute("orderDetailNumber", orderDetailNumber);
+////		req.setAttribute("reviewDate", reviewDate);
+//		req.setAttribute("menuName", menuName);
 		
 		try {
 			req.getRequestDispatcher("review/modMyReviewForm.tiles").forward(req, resp);
